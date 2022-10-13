898e5882a9e05395a9618fbdfea65c58ce64fd19
Ronald <ronald.isael@hotmail.com>
2022-10-13 17:06:57 -0400

    Se agrego las llaves publica y privada para la conexion a la api de marvel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+//public key: cc90b6307b4b0cca6c083afc0b4bb326
+//privary key: 18662b5aff20448f99e57f972fc85ee533dfd497
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' >
+      <h1>Marvel</h1>
     </div>
   );
 }
