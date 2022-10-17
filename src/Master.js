febbc7477d159a11f667dcfd2b6d8986cf4c0f0a
Ronald <ronald.isael@hotmail.com>
2022-10-16 21:57:52 -0400

    Navigation con Wavy Transtions aun sin poderle darle estilo

---
+++ src/Master.js
@@ -0,0 +1,48 @@
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import '../src/Master.css'
+import { WavyContainer, WavyLink } from "react-wavy-transitions";
+import App from './App';
+
+const Home = () => <div><App/></div>
+const About = () => <div>About</div>
+const Contact = () => <div>Contact</div>
+const Hello =() => <div>Hello World!!!</div>
+
+function Master() {
+    return (
+        <BrowserRouter>
+            <WavyContainer />
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <>
+                        
+                            <WavyLink to="/" color="#ff44fd">
+                                Home
+                            </WavyLink>
+                            <WavyLink direction="up" to="/about" color="#8f44fd">
+                                About
+                            </WavyLink>
+                            <WavyLink duration={1000} to="/contact" color="#2f44fd">
+                                Contact
+                            </WavyLink>
+                            <WavyLink to='/hello'>Hello</WavyLink>
+                            <Outlet />
+                            
+                        </>
+                    }
+                className="Nav">
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="hello" element={<Hello/>}/>
+                    <Route path="*" element={<>No Match</>} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+
+export default Master;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Cards from './Cards';
+import Master from './Master';
+import Hello from './Hello';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Master/>
   </React.StrictMode>
 );
 
