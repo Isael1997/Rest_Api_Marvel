b2f37bbcaf44c3696c55232a0e93273ab3f81335
Ronald <ronald.isael@hotmail.com>
2022-10-18 14:07:25 -0400

    Se disenio con algunas margenes en las cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,25 +1,24 @@
 import React from "react";
-import image from '../assets/image1.jpg';
 
 
-export default function Cards(){
+export default function Cards({imageSource, title, url}){
     return (
         <div className="card text-center bg-dark ">
         <div className="overflow">
-            <img src={image} alt="a wallpaper" className="card-img-top" />
+            <img src={imageSource} alt="a wallpaper" className="card-img-top" />
         </div>
         <div className="card-body text-light">
-            <h4 className="card-title"></h4>
+            <h4 className="card-title">{title}</h4>
             <p className="card-text text-secondary">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem possimus non nostrum beatae amet dolor aut iusto ab incidunt vero laudantium provident quisquam commodi, molestiae consequatur in nam asperiores voluptate. Voluptates sit iste esse vero, dolorum, nihil reprehenderit praesentium fugiat veritatis, quod exercitationem aspernatur doloribus beatae itaque eveniet illo nobis?
             </p>
             <a
-                href='#'
+                href={url}
                 target="_blank"
                 className="btn btn-primary border-0"
                 rel="noreferrer"
             >
-                Go to
+                Go to {title}
             </a>
         </div>
     </div>
