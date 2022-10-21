ba433e5b55d6f59938b0d8b3c742afc8e3165af9
Ronald <ronald.isael@hotmail.com>
2022-10-21 13:02:04 -0400

    Funciona la pagina de Rick and Morty

--- src/pages/Page_rick_and_morty.js
+++ src/pages/Page_rick_and_morty.js
@@ -1,24 +1,88 @@
 import { useEffect, useState } from "react";
 
-export default function Page() {
+function NavPage({ page, setPage }) {
+    const Apage = () => {
+        if (page === 1) {
+            setPage(1);
+            alert("NO hay mas paginas");
+            console.log("Pagina: ", { page })
+        }
+        else {
+            setPage(page - 1);
+        }
 
+
+
+    }
+
+
+    return (
+        <header className="d-flex justify-content-between align-items-center">
+
+            <button
+                className="btn btn-primary btn-sm"
+                onClick={Apage}
+            >
+                Page {page - 1}
+            </button>
+
+            <p>Page: {page}</p>
+            <button
+                className="btn btn-primary btn-sm"
+                onClick={() => setPage(page + 1)}
+            >
+                Page {page + 1}
+            </button>
+        </header >
+    );
+}
+
+export default function Page() {
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState([true]);
+    const [page, setPage] = useState(1);
     useEffect(() => {
-        const [characters, setCharacters] = useState([]);
+
         async function conexion() {
-            const url = "https://rickandmortyapi.com/api/character";
+            const url = `https://rickandmortyapi.com/api/character?page=${page}`;
             const res = await fetch(url);
             const data = await res.json();
-            console.log(data);
-            console.log(data.results)
-            setCharacters(data.results);
+            const { results } = await data;
+            setCharacters(results);
+            setLoading(false);
+
+            console.log(data)
+
         }
 
         conexion();
-    },[]);
+    }, [page]);
 
     return (
         <div>
             <h1>Rick and Morty</h1>
-        </div>
+            <div className="container">
+                <NavPage page={page} setPage={setPage} />
+
+                {loading ? (
+                    <div>Loading .....</div>
+                ) : (
+                    < div className="row">
+                        {characters.map(char => {
+                            return (
+                                <div className="col-md-4" key={char.id}>
+                                    <h3>{char.name}</h3>
+                                    <img src={char.image} alt={char.name} className="img-fluid rounded-pill" />
+                                    <p>{`Origin: ${char.origin && char.origin.name}`}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                )}
+                <NavPage page={page} setPage={setPage} />
+            </div>
+
+
+        </div >
     )
 }
